856bab0043a7c714f0e0e666b0833485f99702dc
laz <laz@laz-HP-Notebook>
2023-09-18 17:55:29 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Events
+
+test
+
+#### License
+
+MIT
---
+++ event_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Events",
+			"type": "module",
+			"label": _("Events")
+		}
+	]
