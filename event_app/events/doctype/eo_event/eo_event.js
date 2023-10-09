e1e9cf8a87c26b65c8f81507922311ef75c4d37f
Alazr <alazerb2019@gmail.com>
2023-10-09 17:46:08 +0530

    feat:task-00451:event doctype added

---
+++ event_app/events/doctype/eo_event/eo_event.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, alazar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Eo-Event", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ event_app/events/doctype/eo_event/test_eo_event.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, alazar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEoEvent(FrappeTestCase):
+	pass
